c685fdc9d9589e6a8f40196ccceb4424763cbe40
ANIKET <raneaniket23@gmail.com>
2025-11-03 05:36:17 +0000

    ch

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       NODE_ENV: 'production',
       APP_URL: 'https://crm.maulicardecor.com',
       SESSION_SECRET: '8pSnCe9YF1FehlBI1YcX1Z2Z6r90x7zRd0yBM+CPTZaGwkurNBDzybjgretUTO4l9LT7wRLZln1jqnpqjtKECw==',
-      MONGODB_URI: 'mongodb+srv://raneaniket23_db_user:c51rYLvbIEDGX1qc@autocrm.fuz97x1.mongodb.net/?retryWrites=true&w=majority&appName=AUTOCRM',
+      MONGODB_URI: 'mongodb://localhost:27017/autocrm',
       WHATSAPP_PHONE_NUMBER_ID: '919970127778',
       WHATSAPP_API_KEY: '7RlFwj57xE6wHngTfSmNHA',
       PORT: '5000'
